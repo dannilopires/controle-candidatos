536bed8f2e6128c969f0020c20d558c9696957f1
dannilo <dannilo-pires@hotmail.com>
2023-10-10 12:06:12 -0300

    add metodo valorpretendido e selecaocandidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,13 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
         System.out.println("Processo seletivo");
 
         analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
 
     static void analisarCandidato(double salarioPretendido){
@@ -16,6 +19,31 @@ public class ProcessoSeletivo {
         } else {
             System.out.println("Aguardando demais candidatos");
         }
+    }
 
+    static void selecaoCandidatos(){
+        String[] candidatos={"Ana", "Ivo", "João", "Otto", "Maia", "Lua", "Pedro", "Luana"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados<5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            
+            System.out.println("O(A) candidato(a) " + candidato + " solicitou " + salarioPretendido);
+
+            if (salarioBase>=salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado");
+                candidatosSelecionados++;
+            }
+
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
 }
