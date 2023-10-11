c9b97013e0cc29be0678df1fc6cd8c585eeb46ef
dannilo <dannilo-pires@hotmail.com>
2023-10-11 10:26:52 -0300

    add método ligarCandidato

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,14 +1,21 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        System.out.println("PROCESSO SELETIVO");
+        String[] candidatos={"Ana", "Ivo", "João", "Otto", "Maia"};
 
         analisarCandidato(2000.0);
         selecaoCandidatos();
         imprimirSelecionados();
+
+        for(String c : candidatos){
+            ligarCandidato(c);
+        }
+
+
     }
 
     static void analisarCandidato(double salarioPretendido){
@@ -44,6 +51,7 @@ public class ProcessoSeletivo {
         }
     }
 
+    //método auxiliar
     static double valorPretendido() {
         return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
@@ -53,6 +61,32 @@ public class ProcessoSeletivo {
 
         for (int i=0; i<candidatosSelecionados.length; i++) {
             System.out.println("O candidato de nº " + (i+1) + " é o candidato " + candidatosSelecionados[i] );
+        }
+    }
+
+    //método auxiliar
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+
+    static void ligarCandidato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu= atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("Contato realizado com sucesso!");
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu){
+            System.out.println("Conseguimos entrar em contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+        } else {
+            System.out.println("Não conseguimos entrar em contato com " + candidato + ". Número máximo de tentativa " + tentativasRealizadas);
         }
     }
 }
